70f33807e93a49c18b8fadf91bcb8f585178552c
David Rose <david@drose.io>
2024-04-29 15:57:33 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChatInterface from './ChatInterface';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Zoe</h1>
+      <ChatInterface />
+    </div>
+  );
+};
+
+export default App;
